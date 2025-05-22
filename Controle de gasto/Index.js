cfb3c52e5cf1d4bd671548c1eb8e8d94d0b062e5
Gustavo <131564127+gustavosantt@users.noreply.github.com>
2025-05-22 10:19:17 -0300

    Add files via upload

---
+++ Controle de gasto/Index.js
@@ -0,0 +1,12 @@
+const prev = document.getElementById('Despesa');
+
+//Enviar orçamento previnindo o envio
+prev.addEventListener('submit', function(e){
+    e.preventDefault();
+
+}
+)
+//Enviar despesa para o formulario
+function Enviardespesa  (){
+    alert ("Depesa enviada");
+}
